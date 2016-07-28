7887b96f72699daf1471caaf890e6877cf70ccad
Radeg <radekbenes@centrum.cz>
2016-07-28 17:38:52 +0200

    28-07-16

---
+++ app/pages/detail-statistic/detail-statistic.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/detail-statistic/detail-statistic.html'
+})
+export class DetailStatistic {
+
+  constructor(private navCtrl: NavController) {
+  }
+}
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {DetailStatistic} from '../detail-statistic/detail-statistic';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html',
+  templateUrl: 'build/pages/home/home.html'
 })
+
 export class Home {
 
   constructor(private navCtrl: NavController) {
+
+  }
+
+  detailStatistic() {
+    this.navCtrl.push(DetailStatistic);
   }
 }
